693ace4406bd13132d34a238c901a7a7b6687b5a
Kang Taehee <dev.thk28@gmail.com>
2022-05-11 11:47:46 +0900

    refactor: viewModel의 todoItems 수정
    - MutableStateFlow -> StateFlow
    - useCase에서 받은 Flow에 stateIn 사용해서 StateFlow로 변환하도록 수정

--- presentation/src/main/java/com/thk/cleantodo/TodoViewModel.kt
+++ presentation/src/main/java/com/thk/cleantodo/TodoViewModel.kt
@@ -12,9 +12,7 @@ import com.thk.domain.GetTodoListUseCase
 import com.thk.domain.SetCompletedUseCase
 import com.thk.domain.Todo
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -25,16 +23,23 @@ class TodoViewModel @Inject constructor(
     private val setCompletedUseCase: SetCompletedUseCase
 ) : ViewModel() {
 
-    var todoItems = MutableStateFlow<List<Todo>>(emptyList())
-        private set
+    val todoItems: StateFlow<List<Todo>> = getTodoListUseCase.invoke().stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(),
+        initialValue = emptyList()
+    )
+
+//    var todoItems = MutableStateFlow<List<Todo>>(emptyList())
+//        private set
 
     init {
-        viewModelScope.launch {
-            getTodoListUseCase.invoke().collect {
-                todoItems.emit(it)
-            }
-        }
+//        viewModelScope.launch {
+//            getTodoListUseCase.invoke().collect {
+//                todoItems.emit(it)
+//            }
+//        }
     }
+
 
     fun addNewTodo(content: String) {
         // TODO: 새로운 todo의 인스턴스를 만드는건 UseCase로 넘기기

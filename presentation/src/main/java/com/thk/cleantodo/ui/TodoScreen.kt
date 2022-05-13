0e750330da8a8817774f7a395869414bc3a0fe22
Kang Taehee <dev.thk28@gmail.com>
2022-05-13 15:53:59 +0900

    refactor: swipeable을 DismissToSwipe로 변경
    - 디테일 더 수정해야 함

--- presentation/src/main/java/com/thk/cleantodo/ui/TodoScreen.kt
+++ presentation/src/main/java/com/thk/cleantodo/ui/TodoScreen.kt
@@ -1,7 +1,11 @@
-@file:OptIn(ExperimentalMaterial3Api::class)
+@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
 
 package com.thk.cleantodo.ui
 
+import android.util.Log
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.FlingBehavior
 import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.gestures.detectDragGestures
@@ -9,17 +13,26 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.material.FractionalThreshold
+import androidx.compose.material.*
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Send
-import androidx.compose.material.swipeable
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.material3.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
@@ -128,55 +141,76 @@ fun TodoRow(
     todo: Todo,
     onCheckCompleted: (Todo) -> Unit
 ) {
-    val swipeableState = androidx.compose.material.rememberSwipeableState(initialValue = 0)
-    // 앵커로 사용할 위치
-    val point = LocalDensity.current.run { LocalConfiguration.current.screenWidthDp.dp.toPx() }
-    // 위치 to 상태
-    val anchors = mapOf(0f to 0, -point to 1)
-
-    if (swipeableState.isAnimationRunning) {
-        DisposableEffect(Unit) {
-            onDispose {
-                if (swipeableState.currentValue == 1) {
-                    // do delete
-                }
+    val dismissState = rememberDismissState(confirmStateChange = {
+        when (it) {
+            DismissValue.Default -> false
+            DismissValue.DismissedToStart -> {
+                Log.d("TAG", "TodoRow: DismissedToStart")
+                true
             }
+            else -> false
         }
-    }
+    })
+    
+    SwipeToDismiss(
+        state = dismissState,
+        directions = setOf(DismissDirection.EndToStart),
+        background = {
+            val direction = dismissState.dismissDirection ?: return@SwipeToDismiss
 
-    Box(
-        Modifier
-            .fillMaxWidth()
-            .swipeable(
-                state = swipeableState,
-                orientation = Orientation.Horizontal,
-                anchors = anchors,
-                thresholds = { _, _ -> FractionalThreshold(0.8f) },
-                velocityThreshold = 1000.dp     // TODO: 실물 디바이스에서 어느정도가 적절한지 테스트하기
-            )
-    ) {
-        Card(
-            modifier = Modifier
-                .offset { IntOffset(swipeableState.offset.value.roundToInt(), 0) }
-                .fillMaxWidth()
-                .padding(vertical = 4.dp),
-            shape = RoundedCornerShape(16.dp)
-        ) {
+            Log.d("TAG", "TodoRow: ${dismissState.progress}")
 
-            Row(verticalAlignment = Alignment.CenterVertically) {
-                Checkbox(
-                    checked = todo.isCompleted,
-                    onCheckedChange = {
-                        onCheckCompleted(todo.copy(isCompleted = it))
+            
+//            val backgroundColor by animateColorAsState(targetValue = Color.Red.copy(alpha = dismissState.progress.fraction))
+            // FIXME: 더 괜찮은 방법 찾기
+            val alpha by animateFloatAsState(
+                targetValue = when(direction) {
+                    DismissDirection.EndToStart -> {
+                        dismissState.progress.fraction.run {
+                            if (this < 0.7f) this else (1f - this)
+                        }
                     }
-                )
+                    else -> 0f
+                }
+            )
 
-                Text(
-                    text = todo.content,
-                    textDecoration = if (todo.isCompleted) TextDecoration.LineThrough else TextDecoration.None
-                )
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()
+                    .padding(vertical = 4.dp)
+                    .background(Color.Red.copy(alpha = alpha))
+                    .padding(end = 16.dp)
+                    .clip(RoundedCornerShape(16.dp)),
+                contentAlignment = Alignment.CenterEnd
+            ) {
+                Icon(imageVector = Icons.Outlined.Delete, contentDescription = "delete")
             }
+        },
+        dismissContent = {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 4.dp),
+                shape = RoundedCornerShape(16.dp)
+            ) {
 
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    Checkbox(
+                        checked = todo.isCompleted,
+                        onCheckedChange = {
+                            onCheckCompleted(todo.copy(isCompleted = it))
+                        }
+                    )
+
+                    Text(
+                        text = todo.content,
+                        textDecoration = if (todo.isCompleted) TextDecoration.LineThrough else TextDecoration.None
+                    )
+                }
+
+            }
         }
-    }
+    )
+
 }

ae62dc4419c713a39962ad7a35f58f0a87fdb7ac
taehee28 <dev.thk28@gmail.com>
2022-05-25 19:55:58 +0900

    feat: swipe 메뉴 기능
    * longClick으로 수정하기 기능 삭제
    * 기존의 SwipeToDismiss에서 Swipeable로 변경
    * swipe했을 때 끝까지 밀려지지 않고 anchor에 걸리도록 변경
    * swipe하면 수정, 삭제 버튼이 보이도록 변경
    
    + 컴포저블 코드 refactoring 필요

--- presentation/src/main/java/com/thk/cleantodo/ui/TodoScreen.kt
+++ presentation/src/main/java/com/thk/cleantodo/ui/TodoScreen.kt
@@ -8,6 +8,7 @@ import android.util.Log
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.*
+import androidx.compose.foundation.gestures.Orientation
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -22,8 +23,10 @@ import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material3.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -36,18 +39,28 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.focus.*
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
+import androidx.core.graphics.drawable.DrawableCompat
+import com.thk.cleantodo.R
 import com.thk.cleantodo.ui.theme.CleanTodoTheme
 import com.thk.domain.Todo
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
 @Composable
 fun TodoScreen(
@@ -203,7 +216,7 @@ fun TodoList(
         state = scrollState
     ) {
         items(todoItems, { it.num }) {
-            SwipeableRow(
+            SwipeableMenuRow(
                 todo = it,
                 onCheckCompleted = onCheckCompleted,
                 onDeleteTodo = onDeleteTodo,
@@ -213,7 +226,6 @@ fun TodoList(
         }
     }
 }
-
 
 @Composable
 fun SwipeableRow(
@@ -267,32 +279,116 @@ fun SwipeableRow(
             TodoRow(
                 todo = todo,
                 onCheckCompleted = onCheckCompleted,
-                onEditStart = onEditStart
             )
         }
     )
 }
 
 @Composable
+fun SwipeableMenuRow(
+    todo: Todo,
+    onCheckCompleted: (Todo) -> Unit,
+    onDeleteTodo: (Todo) -> Unit,
+    onEditStart: (Todo) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val swipeableState = rememberSwipeableState(initialValue = 0)
+    val anchor = LocalDensity.current.run { 120.dp.toPx() }
+    val anchors = mapOf(0f to 0, -anchor to 1)
+
+    val scope = rememberCoroutineScope()
+
+
+    Box(
+        modifier = modifier
+            .fillMaxWidth()
+            .swipeable(
+                state = swipeableState,
+                orientation = Orientation.Horizontal,
+                anchors = anchors
+            )
+    ) {
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(64.dp)
+                .padding(vertical = 4.dp),
+            horizontalArrangement = Arrangement.End
+        ) {
+
+
+            MenuButton(
+                icon = painterResource(id = R.drawable.ic_edit),
+                contentDescription = "edit",
+                backgroundColor = Color(0xFFC4DEFF),
+                onClick = {
+                    scope.launch { swipeableState.animateTo(targetValue = 0) }
+                    onEditStart(todo)
+                }
+            )
+
+            MenuButton(
+                icon = painterResource(id = R.drawable.ic_delete),
+                contentDescription = "delete",
+                backgroundColor = Color(0xFFFFB9B9),
+                onClick = { onDeleteTodo(todo) }
+            )
+
+        }
+
+        TodoRow(
+            todo = todo,
+            onCheckCompleted = onCheckCompleted,
+            modifier = Modifier.offset { IntOffset(swipeableState.offset.value.roundToInt(), 0) }
+        )
+    }
+
+}
+
+@Composable
+fun MenuButton(
+    icon: Painter,
+    contentDescription: String = "button",
+    backgroundColor: Color = Color.White,
+    onClick: () -> Unit
+) {
+
+    androidx.compose.material.IconButton(
+        modifier = Modifier
+            .width(60.dp)
+            .height(64.dp)
+            .clip(RoundedCornerShape(16.dp))
+            .background(backgroundColor),
+        onClick = onClick
+    ) {
+        Icon(
+            painter = icon,
+            contentDescription = contentDescription,
+            Modifier
+                .scale(0.8f)
+        )
+    }
+}
+
+@Composable
 fun TodoRow(
     todo: Todo,
     onCheckCompleted: (Todo) -> Unit,
-    onEditStart: (Todo) -> Unit
+    modifier: Modifier = Modifier
 ) {
     Card(
-        modifier = Modifier
+        modifier = modifier
             .fillMaxWidth()
-            .padding(vertical = 4.dp)
-            .combinedClickable(
-                onClick = {},
-                onLongClick = {
-                    onEditStart(todo)
-                }
-            ),
+            .height(64.dp)
+            .padding(vertical = 4.dp),
         shape = RoundedCornerShape(16.dp)
     ) {
 
-        Row(verticalAlignment = Alignment.CenterVertically) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxSize()
+        ) {
             Checkbox(
                 checked = todo.isCompleted,
                 onCheckedChange = {
